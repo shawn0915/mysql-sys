9bc4e5e4b57d0b82958232485ba564c5f0d657fe
ShawnYan <shawn2016@aliyun.com>
2018-09-21 17:47:49 +0800

    upgrade version to 1.7.0

--- sys_Percona-5.6.sql
+++
@@ -1,3 +0,0 @@
--- Alias for sys_56
-
-SOURCE ./sys_56.sql
--- views/version.sql
+++ views/version.sql
@@ -33,6 +33,6 @@ VIEW version (
   sys_version,
   mysql_version
 ) AS 
-SELECT '1.6.2' AS sys_version, 
+SELECT '1.7.0' AS sys_version, 
         version() AS mysql_version;
 
